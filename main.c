cfd6fa0d9cf47fec0ec738b284fbc0e95c5391a9
antoniettaSantoro <a.santoro141@studenti.unisa.it>
2026-05-05 20:52:42 +0200

    feat: aggiunta delle funzioni print_menu al main

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "item.h"
+
+void print_menu(){
+
+    printf("=====================================\n");
+    printf("\t\t\tMENU\n");
+    printf("=====================================\n");
+    printf("0 - Esci\n");
+    printf("1 - Registra Segnalazione\n");
+    printf("2 - Elimina Segnalazione\n");
+    printf("3 - Visualizza Tutte le Segnalazioni\n");
+    printf("4 - Ricerca Segnalazione\n");
+    printf("5 - Aggiorna Stato Segnalazione\n");
+    printf("6 - Visualizza Segnalazioni per Stato\n");
+    printf("7 - Visualizza Segnalazioni più Urgenti\n");
+    printf("8 - Genera Report\n");
+    printf("=====================================\n");
+    printf("\n\n");
+
+}
+
+void print_menu_ricerca(){
+
+    printf("=====================================\n");
+    printf("\t\t\tRICERCA\n");
+    printf("=====================================\n");
+    printf("0 - Indietro\n");
+    printf("1 - Ricerca per ID\n");
+    printf("2 - Ricerca per Categoria\n");
+    printf("=====================================\n");
+    printf("\n\n");
+}
+
+void print_menu_visualizza_stato(){
+
+    printf("=====================================\n");
+    printf("\t\t\t VISUALIZZAZIONE\n");
+    printf("=====================================\n");
+    printf("0 - Indietro\n");
+    printf("1 - Segnalazioni Aperte\n");
+    printf("2 - Segnalazioni Chiuse\n");
+    printf("3 - Segnalazioni In Lavorazione\n");
+    printf("=====================================\n");
+    printf("\n\n");
+
+}
+
+int main(){
+
+
+
+
+}
