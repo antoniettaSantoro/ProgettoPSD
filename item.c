65ce7c18a3f89c142ce16ebdef0e04a071870554
antoniettaSantoro <a.santoro141@studenti.unisa.it>
2026-05-05 21:33:37 +0200

    feat: aggiunta dell'ADT item (segnalazione) con funzioni base: creazione, eliminazione, get, aggiorna_stato

---
+++ item.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "item.h"
+
+struct segnalazione {
+    int id;
+    char nome[51];
+    categoria cat;
+    char descrizione[101];
+    char data[10];             
+    int urgenza;
+    stato st;
+};
+
+/****Gestione della Memoria****/
+
+item crea_segnalazione(int id, char* nome, categoria cat, char* descrizione, char* data, int urgenza, stato st){
+    item s = (item) malloc(sizeof(struct segnalazione));
+    if(s == NULL)
+        return NULL;    //ritorna NULL se ci sono errori durante l'allocazione della memoria
+
+    s->id = id;
+    s->cat = cat;
+    s->urgenza = urgenza;
+    s->st = st;
+
+    strncopy(s->nome, nome, sizeof(s->nome) - 1);
+    s->nome[sizeof(s->nome) - 1] = '\0';
+
+    strncopy(s->descrizione, descrizione, sizeof(s->descrizione) - 1);
+    s->descrizione[sizeof(s->descrizione) - 1] = '\0';
+
+    strncopy(s->data, data, sizeof(s->data) - 1);
+    s->data[sizeof(s->data) - 1] = '\0';
+    
+    return s;
+}
+
+void libera_segnalazione(item s){
+    free(s);
+}
+
+/****Get****/
+
+int get_id(item s){
+    return s->id;
+}
+
+char* get_nome(item s){
+    return s->nome;
+}
+
+categoria get_cat(item s){
+    return s->cat;
+}
+
+char* get_descrizione(item s){
+    return s->descrizione;
+}
+
+char* get_data(item s){
+    return s->data;
+}
+
+int get_urgenza(item s){
+    return s->urgenza;
+}
+
+stato get_stato(item s){
+    return s->st;
+}
+
+/*Stato*/
+
+void aggiorna_stato(item s, stato st){
+    s->st = st;
+}
---
+++ item.h
@@ -0,0 +1,44 @@
+#define NULLITEM 0
+
+/*ILLUMINAZIONE = 0, GUASTI = 1, RIFIUTI = 2, STRADE = 3*/
+typedef enum{
+    ILLUMINAZIONE,
+    GUASTI,
+    RIFIUTI,
+    STRADE
+} categoria;
+
+/*APERTO = 0, CHIUSO = 1, INLAVORAZIONE = 2*/
+typedef enum{
+    APERTO,
+    CHIUSO,
+    INLAVORAZIONE
+} stato;
+
+typedef struct segnalazione *item;
+
+/****Gestione della Memoria****/
+
+//Crea una nuova segnalazione e restituisce un puntatore ad essa.
+//Prende in input; id, nome del cittadino, categoria del problema, descrizione, data di inserimento, livello di urgenza, stato della segnalazione
+//Nome max 50 caratteri, Descrizione max 100 caratteri
+item crea_segnalazione(int id, char* nome, categoria cat, char* descrizione, char* data, int urgenza, stato st);
+
+//Elimina e libera la memoria dato un puntatore a segnalazione in input
+void libera_segnalazione(item s);
+
+/****Get****/
+//Queste funzioni restituiscono i parametri di una segnalazione data in input
+
+int get_id(item s);
+char* get_nome(item s);
+categoria get_cat(item s);
+char* get_descrizione(item s);
+char* get_data(item s);
+int get_urgenza(item s);
+stato get_stato(item s);
+
+/****Stato****/
+
+//Modifica il valore dello stato nella segnalazione data in input
+void aggiorna_stato(item s, stato st);
